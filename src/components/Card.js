60fe1aebe99f94ad67e3fbc6869c3305e40502d4
AdyMoko <mocodean_adrian@yahoo.com>
2022-05-19 06:32:17 +0300

    fixed a bug where the API call to robohash would receive a response of 500

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id, randomChars}) => {
   return (
     <div className='hover-bg-light-yellow tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}${randomChars}3?size=300x300`} />
+      <img alt='robots' src={`https://robohash.org/${id}${randomChars}`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
